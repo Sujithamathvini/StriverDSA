683b90a556c4e4a8ccc1aa4479bdc9917c0d9028
Sujithamathvini <70484186+Sujithamathvini@users.noreply.github.com>
2022-09-26 11:49:29 +0530

    Create DNFSort.cpp

---
+++ Day1-Arrays/DNFSort.cpp
@@ -0,0 +1,26 @@
+//problem link - https://leetcode.com/problems/find-the-duplicate-number/
+//Approach1 - sorting
+//Approach2 - count 0,1,2
+
+//Approach3 - 3 pointer
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+    int l=0,m=0,h=n-1;
+    while(m<=h){
+        switch(arr[m]){
+                case 0:
+                    swap(arr[l++],arr[m++]);
+                    break;
+                case 1:
+                    m++;
+                    break;
+                case 2:
+                    swap(arr[m],arr[h--]);
+                    break;
+        }
+    }
+}
+//Time complexity - O(n)
+//Space Complexity - O(1)
