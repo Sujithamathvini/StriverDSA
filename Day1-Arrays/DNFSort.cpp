9175fccd5290084a5bf711c111e4354548e4e2b9
Sujithamathvini <70484186+Sujithamathvini@users.noreply.github.com>
2022-09-26 11:59:14 +0530

    Update DNFSort.cpp

--- Day1-Arrays/DNFSort.cpp
+++ Day1-Arrays/DNFSort.cpp
@@ -1,8 +1,13 @@
-//problem link - https://leetcode.com/problems/sort-colors/
-//Approach1 - sorting
-//Approach2 - count 0,1,2
+/*
+problem link - https://leetcode.com/problems/sort-colors/
+Approach1 - sorting
+Approach2 - count 0's,1's,2's
 
-//Approach3 - 3 pointer
+Approach3 - 3 pointer
+Time complexity - O(n)
+Space Complexity - O(1)
+*/
+
 #include <bits/stdc++.h> 
 void sort012(int *arr, int n)
 {
@@ -10,17 +15,15 @@ void sort012(int *arr, int n)
     int l=0,m=0,h=n-1;
     while(m<=h){
         switch(arr[m]){
-                case 0:
+                case 0://when a[m]==0
                     swap(arr[l++],arr[m++]);
                     break;
-                case 1:
+                case 1://when a[m]==1
                     m++;
                     break;
-                case 2:
+                case 2://when a[m]==2
                     swap(arr[m],arr[h--]);
                     break;
         }
     }
 }
-//Time complexity - O(n)
-//Space Complexity - O(1)
