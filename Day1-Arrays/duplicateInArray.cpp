66722c685a948d630248910657d8c8b861418e35
Sujithamathvini <70484186+Sujithamathvini@users.noreply.github.com>
2022-09-26 11:57:13 +0530

    Update duplicateInArray.cpp

--- Day1-Arrays/duplicateInArray.cpp
+++ Day1-Arrays/duplicateInArray.cpp
@@ -1,8 +1,14 @@
-//poblem link - https://leetcode.com/problems/find-the-duplicate-number/
-//Approach1 - Sorting //time complexity - O(nlogn + n) , space complexity - O(1)
-//Approach2 - using freq array //time complexity - O(n) , space complexity - O(n)
+/*
+problem link - https://leetcode.com/problems/find-the-duplicate-number/
 
-//Approach3 - Floyd Warshall Algo
+Approach1 - Sorting //time complexity - O(nlogn + n) , space complexity - O(1)
+Approach2 - using freq array //time complexity - O(n) , space complexity - O(n)
+
+Approach3 - Floyd Warshall Algo
+time complexity - O(n)
+space complexity - O(1)
+*/
+
 int findDuplicate(vector<int> &a, int n){
 int s=a[0];
     int f=a[0];
@@ -17,5 +23,3 @@ int s=a[0];
     }
     return f;//when the s and f meet eachother that number is the duplicate number
 }
-//time complexity - O(n)
-//space complexity - O(1)
