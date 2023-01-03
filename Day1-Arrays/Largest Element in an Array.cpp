a4f6f3ba6a2bcacd406f3609f0905f5e16df70f3
Sujithamathvini <70484186+Sujithamathvini@users.noreply.github.com>
2023-01-03 16:29:33 +0530

    Create Largest Element in an Array.cpp

---
+++ Day1-Arrays/Largest Element in an Array.cpp
@@ -0,0 +1,52 @@
+/***
+Find the Largest element in an array
+Problem Statement: Given an array, we have to find the largest element in the array.
+
+Examples:
+
+Example 1:
+Input: arr[] = {2,5,1,3,0};
+Output: 5
+Explanation: 5 is the largest element in the array. 
+
+Example2: 
+Input: arr[] = {8,10,5,7,9};
+Output: 10
+Explanation: 10 is the largest element in the array. 
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int largest(vector<int> &a, int n)
+    {
+        int max=a[0];
+        for(int i=0;i<n;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        return max;
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
